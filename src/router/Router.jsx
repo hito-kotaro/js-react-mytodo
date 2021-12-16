9035e11f9cba57e6cd418d095b837cb49f9ffda9
hito-kotaro <tohi.work@gmail.com>
2021-12-16 20:09:56 +0900

    Recoilのimport場所を変更

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,21 +1,18 @@
 import React from 'react';
-import { RecoilRoot } from 'recoil';
 import { Route, Routes } from 'react-router-dom';
 import TodoApp from '../components/TodoList/TodoApp/TodoApp';
 import TodoDetail from '../components/TodoList/TodoDetail/TodoDetail';
 
 function Router() {
   return (
-    <RecoilRoot>
-      <Routes>
-        <Route exact path="/">
-          <Route path="" element={<TodoApp />} />
-        </Route>
-        <Route exact path="detail">
-          <Route path="" element={<TodoDetail />} />
-        </Route>
-      </Routes>
-    </RecoilRoot>
+    <Routes>
+      <Route exact path="/">
+        <Route path="" element={<TodoApp />} />
+      </Route>
+      <Route exact path="detail">
+        <Route path="" element={<TodoDetail />} />
+      </Route>
+    </Routes>
   );
 }
 
